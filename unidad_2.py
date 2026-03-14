ad7c855ee400913b918cd1fe7e48055410e17db6
Javier Penas <javichin@MacBook-Air-de-Javier.local>
2026-03-14 18:20:59 +0100

    Tarea terminada

--- unidad_2.py
+++ unidad_2.py
@@ -1,4 +1,6 @@
 from faker import Faker
+import random
+
 fake = Faker()
 
 personas = {}
@@ -6,10 +8,8 @@ personas = {}
 for n in range (15):
     personas[n+1] = {"nombre": fake.name(), "direccion": fake.address(), "correo_electronico": fake.email(), "telefono": fake.phone_number()}
 
-
-
-
-
-
-
 print(personas)
+
+print()
+
+print(f"El usuario llamado {personas[random.randint(1, 15)]["nombre"]} es el afortunado.")
